ceb8b9a8c48e499b68a964935cdaa05b3eade714
shintanr <91821725+shintanr@users.noreply.github.com>
2024-06-01 23:04:48 +0700

    update code invalid token

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/api/ApiConfig.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/api/ApiConfig.kt
@@ -9,11 +9,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object {
-        fun getApiService(): ApiService {
+        fun getApiService(token: String): ApiService {
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val authInterceptor = Interceptor { chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(requestHeaders)
+            }
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BuildConfig.STORYAPI_BASE_URL)
